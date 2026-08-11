f788fef7c2c7d902bb1ad01fedafddf996ab537b
Jack <jackkleyk@gmail.com>
2026-08-11 11:57:09 +1000

    continued working on automatic mode and started line follow mode

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,22 @@
 #define pinRightLED 12
 #define pinServo 25
 #define pinBuzzer 33
+#define pinIRLeft 35
+#define pinIRMiddle 36
+#define pinIRRight 39
 
 IRrecv irrecv(pinIr);
 
 vehicle myCar;
 
+ultrasonic myUltrasonic;
+
 Servo servo;
 
-float rRotateIndex = 1;
-float lRotateIndex = 1;
-
 int stopSec = 0;
+int mode = 2;
+
+int ultrasonicDistance;
 
 /*
   myCar.Move(Forward, 255);       Forward
@@ -34,94 +39,220 @@ int stopSec = 0;
   myCar.Move(Stop, 0)             Stop
 */
 
-// void buzz() {
-//   tone(pinBuzzer, 262);
-//   tone(pinBuzzer, 494);
-//   noTone(pinBuzzer);
-// }
+/*
+ void buzz() {
+   tone(pinBuzzer, 262);
+   tone(pinBuzzer, 494);
+   noTone(pinBuzzer);
+ }
+*/
 
-void remote() {
-  if (irrecv.decode()) {
-    if (!irrecv.decodedIRData.flags) {
+void led()
+{
+  digitalWrite(pinLeftLED, HIGH);
+  digitalWrite(pinRightLED, LOW);
+  delay(500);
+  digitalWrite(pinLeftLED, LOW);
+  digitalWrite(pinRightLED, HIGH);
+  delay(400);
+}
+
+void ultrasonicSens()
+{
+  ultrasonicDistance = myUltrasonic.Ranging();
+
+  if (ultrasonicDistance < 30)
+  {
+    myCar.Move(Stop, 0);
+  }
+  Serial.println(ultrasonicDistance);
+}
+
+void lineFollow()
+{
+  int left = analogRead(35);
+  int middle = analogRead(36);
+  int right = analogRead(39);
+  Serial.println("Left: ");
+  Serial.println(left);
+  Serial.println("Middle: ");
+  Serial.println(middle);
+  Serial.println("Right ");
+  Serial.println(right);
+
+  if (middle > 2500)
+  {
+    myCar.Move(Forward, 64);
+  }
+  else if (right > 2700 && left < 2700)
+  {
+    myCar.Move(Clockwise, 128);
+    delay(100);
+    myCar.Move(Forward, 32);
+  }
+  else if (left > 2700 && right < 2700)
+  {
+    myCar.Move(Contrarotate, 128);
+    delay(100);
+    myCar.Move(Forward, 32);
+  }
+  myCar.Move(Forward, 128);
+}
+
+void automatic()
+{
+  ultrasonicSens();
+  if (ultrasonicDistance > 40)
+  {
+    myCar.Move(Forward, 128);
+  }
+  else
+  {
+    myCar.Move(Stop, 0);
+    servo.write(135);
+    delay(1000);
+    ultrasonicSens();
+    if (ultrasonicDistance > 50)
+    {
+      myCar.Move(Contrarotate, 128);
+      delay(1000);
+      myCar.Move(Stop, 0);
+    }
+    else
+    {
+      servo.write(45);
+      delay(1000);
+      ultrasonicSens();
+      if (ultrasonicDistance > 50)
+      {
+        myCar.Move(Clockwise, 128);
+        delay(1000);
+        myCar.Move(Stop, 0);
+      }
+      else
+      {
+        myCar.Move(Backward, 128);
+        delay(1000);
+        myCar.Move(Clockwise, 128);
+        delay(1000);
+        myCar.Move(Stop, 0);
+      }
+    }
+  }
+  servo.write(90);
+}
+
+void remote()
+{
+  if (mode == 0)
+  {
+    automatic();
+  }
+  else if (mode == 1)
+  {
+    ultrasonicSens();
+  }
+  else if (mode == 2)
+  {
+    lineFollow();
+  }
+  if (irrecv.decode())
+  {
+    if (!irrecv.decodedIRData.flags)
+    {
       Serial.print("IR Code Received: ");
       int signal = irrecv.decodedIRData.command;
       Serial.println(signal);
       Serial.print("IR Code Received (Hex): 0x");
       Serial.println(irrecv.decodedIRData.command, HEX);
-      if (signal == 70) {
+      if (signal == 70 && mode == 1)
+      {
         myCar.Move(Forward, 128);
-        if (stopSec == 1) {
+        if (stopSec == 1)
+        {
           delay(1000);
           myCar.Move(Stop, 0);
         }
-      } else if (signal == 21) {
-        myCar.Move(Backward, 128);
-        if (stopSec == 1) {
-          delay(1000);
-          myCar.Move(Stop, 0);
-        }
-      } else if (signal == 64 || signal == 82) {
-        myCar.Move(Stop, 0);
-      } else if (signal == 67) {
-        myCar.Move(Move_Right, 128);
-        if (stopSec == 1) {
-          delay(1000);
-          myCar.Move(Stop, 0);
-        }
-      } else if (signal == 68) {
-        myCar.Move(Move_Left, 128);
-        if (stopSec == 1) {
-          delay(1000);
-          myCar.Move(Stop, 0);
-        }
-      } else if (signal == 13) {
-        myCar.Move(Clockwise, 128);
-        if (stopSec == 1) {
-          delay(1000);
-          myCar.Move(Stop, 0);
-        }
-      } else if (signal == 22) {
-        myCar.Move(Contrarotate, 128);
-        if (stopSec == 1) {
-          delay(1000);
-          myCar.Move(Stop, 0);
-        }
-      } else if (signal == 66) {
-          stopSec = 0;
-      } else if (signal == 74) {
-          stopSec = 1;
       }
-      Serial.println(stopSec);
+      else if (signal == 21 && mode == 1)
+      {
+        myCar.Move(Backward, 128);
+        if (stopSec == 1)
+        {
+          delay(1000);
+          myCar.Move(Stop, 0);
+        }
+      }
+      else if ((signal == 64 || signal == 82) && mode == 1)
+      {
+        myCar.Move(Stop, 0);
+      }
+      else if (signal == 67 && mode == 1)
+      {
+        myCar.Move(Move_Right, 128);
+        if (stopSec == 1)
+        {
+          delay(1000);
+          myCar.Move(Stop, 0);
+        }
+      }
+      else if (signal == 68 && mode == 1)
+      {
+        myCar.Move(Move_Left, 128);
+        if (stopSec == 1)
+        {
+          delay(1000);
+          myCar.Move(Stop, 0);
+        }
+      }
+      else if (signal == 13 && mode == 1)
+      {
+        myCar.Move(Clockwise, 128);
+        if (stopSec == 1)
+        {
+          delay(1000);
+          myCar.Move(Stop, 0);
+        }
+      }
+      else if (signal == 22 && (mode == 1))
+      {
+        myCar.Move(Contrarotate, 128);
+        if (stopSec == 1)
+        {
+          delay(1000);
+          myCar.Move(Stop, 0);
+        }
+      }
+      else if (signal == 66)
+      {
+        stopSec = 0;
+      }
+      else if (signal == 74)
+      {
+        stopSec = 1;
+      }
+      else if (signal == 12)
+      {
+        mode = 0;
+      }
+      else if (signal == 24)
+      {
+        mode = 1;
+        myCar.Move(Stop, 0);
+      }
+      else if (signal == 94)
+      {
+        mode = 2;
+      }
+      // Serial.println(stopSec);
     }
     irrecv.resume();
   }
+  // Serial.println(mode);
 }
 
-void led() {
-  digitalWrite(pinLeftLED, HIGH);
-  digitalWrite(pinRightLED, LOW);
-  servo.write(135);
-  delay(500);
-  digitalWrite(pinLeftLED, LOW);
-  digitalWrite(pinRightLED, HIGH);
-  servo.write(45);
-  delay(400);
-}
-
-int rightRotate() {
-  return 1;
-}
-
-int leftRotate() {
-  return 1;
-}
-
-void move() {
-  rRotateIndex = rightRotate();
-  lRotateIndex = leftRotate();
-}
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   servo.attach(pinServo, 500, 2500);
@@ -132,16 +263,23 @@ void setup() {
   pinMode(pinIr, INPUT);
   pinMode(pinRightLED, OUTPUT);
   pinMode(pinBuzzer, OUTPUT);
+  pinMode(pinIRLeft, INPUT);
+  pinMode(pinIRMiddle, INPUT);
+  pinMode(pinIRRight, INPUT);
 
   myCar.Init();
+
+  myUltrasonic.Init(13, 14);
 
   irrecv.enableIRIn();
 }
 
-void loop() {
+void loop()
+{
   remote();
+  // ultrasonicSens();
+  // lineFollow();
   // led();
-  // move();
 
   delay(100);
 }
